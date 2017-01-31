02aa875d6373fc241c33d112f152f050f9dca7c4
Zelret <whdwls7269@naver.com>
2017-01-31 22:03:53 +0900

    Ethan
    move

---
+++ New Unity Project/Assets/charactor/move.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    RaycastHit hit;
+
+    Vector3 click;
+
+    float movespeed;
+
+    //public Animation anime;
+
+    // Use this for initialization
+    void Start () {
+
+        movespeed = 5f;
+
+        //anime.Play("HumanoidWalk");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButton(0))
+        {
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
+            
+            click = hit.point;
+            click.y = 0;
+            
+            transform.Translate((click - transform.position).normalized * movespeed * Time.deltaTime);
+
+            //anime.Play("HumanoidIdle");
+
+        }
+        else
+        {
+            //anime.Play("HumanoidWalk");
+        }
+	}
+}
---
+++ New Unity Project/Assets/charactor/turn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turn : MonoBehaviour {
+
+    RaycastHit hit;
+
+    Vector3 click;
+
+    Quaternion dir;
+
+    float turnspeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        turnspeed = 5f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
+
+            click = hit.point;
+            click.y = 0;
+
+            dir = Quaternion.LookRotation((click - transform.position).normalized);
+
+            dir.x = 0;
+            dir.z = 0;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, dir, turnspeed * Time.deltaTime);
+
+        }
+    }
+}
